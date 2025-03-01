5c976f877f8a7262060faa7f99a7ae5bd1417c67
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-01 11:16:33 -0300

    Add files via upload

--- Produto.java
+++ Produto.java
@@ -1,44 +1,55 @@
 
-package javaex01;
-
 /**
  *
  * @author vitorb
  */
 public class Produto {
+    
+public class Produto {
     private String nome;
     private double preco;
     
-    public Produto (){
-        
+    // Construtor padrão
+    public Produto() {
     }  
- public Produto (String nome){
-     this.nome = nome;
-     
- }
-  public Produto (String nome, double preco){
-      this.nome = nome;
-      this.preco = preco;
-      
-  }
-  
-  public void Exibir() {
-      System.out.println("Nome:" + nome);
-      System.out.println("Preço:" + preco);
-  }
-  public String getNome () {
-      return this.nome;
-  }
-  public void setNome (String nome) {
-      this.nome = nome;
-  }
-  public double getPreco (double preco){
-      return this.preco;
-  } 
-  public void setPreco (double preco){
-      this.preco = preco;
-  }
-  }
+    
+    // Construtor com nome
+    public Produto(String nome) {
+        this.nome = nome;
+    }
 
+    // Construtor com nome e preço
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.setPreco(preco); // Chama o método setPreco para validar o preço
+    }
 
+    // Método para exibir os detalhes do produto
+    public void Exibir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: " + preco);
+    }
+
+    // Métodos Getters e Setters
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    } 
+
+    // Agora o setPreco impede valores negativos
+    public void setPreco(double preco) {
+        if (preco >= 0) {
+            this.preco = preco;
+        } else {
+            System.out.println("Erro: O preço não pode ser negativo!");
+        }
+    }
+}
 
