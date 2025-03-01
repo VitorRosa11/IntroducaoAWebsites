96a8dfc684bf43afcb35d664994d7038f598335c
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-01 11:13:37 -0300

    Add files via upload

---
+++ JavaEx04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projetolistamodificadores.javaex04;
+
+/**
+ *
+ * @author vinic
+ */
+public class JavaEx04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
